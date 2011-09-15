0ffce00e68c30d77db007c4406593690afe1d179
Mike Han <mmhan2u@gmail.com>
2011-09-15 20:59:38 +0800

    First Commit

---
+++ src/ict306lab/MikeHan/labs/lab01/SampleClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ict306lab.MikeHan.labs.lab01;
+
+/**
+ * 
+ * @author      mmhan
+ * @date        2011.09.15
+ * @description This is simply a sample class
+ */
+public class SampleClass {
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+    }
+}
