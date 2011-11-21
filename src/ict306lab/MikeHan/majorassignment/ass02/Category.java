2c53b531f5e3f6cc338304ecc20195fff54b37af
Mike Han <mmhan2u@gmail.com>
2011-11-21 15:11:01 +0800

    An ArrayList was added to keep track of books.

--- src/ict306lab/MikeHan/majorassignment/ass02/Category.java
+++ src/ict306lab/MikeHan/majorassignment/ass02/Category.java
@@ -4,14 +4,66 @@
  */
 package ict306lab.MikeHan.majorassignment.ass02;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author mmhan
  */
 public class Category {
+    /**
+     * Name of the category.
+     */
     String name;
+    /**
+     * To remember the books that are in this category.
+     */
+    ArrayList<Book> books;
+    
+    /**
+     * Constructor
+     * 
+     * @param name 
+     */
     public Category(String name){
         this.name = name;
+        this.books = new ArrayList<Book>();
+    }
+    
+    /**
+     * Add a book to this category
+     * 
+     * @param book
+     * @return 
+     */
+    public boolean add(Book book){
+        return this.books.add(book);
+    }
+    
+    /**
+     * Remove a book from this category
+     * 
+     * @param book
+     * @return 
+     */
+    public boolean remove(Book book){
+        return this.books.remove(book);
+    }
+    
+    /**
+     * Get all books of this category
+     * 
+     * @return 
+     */
+    public Book[] getBooks(){
+        return this.books.toArray(new Book[0]);
+    }
+    /**
+     * Return the number of books this category has.
+     * @return  number of books this category has.
+     */
+    public int count(){
+        return this.books.size();
     }
     
     @Override
--- test/ict306lab/MikeHan/majorassignment/ass02/CategoryTest.java
+++ test/ict306lab/MikeHan/majorassignment/ass02/CategoryTest.java
@@ -46,4 +46,72 @@ public class CategoryTest {
         String result = cat.toString();
         assertEquals(catName, result);
     }
+
+    /**
+     * Test of add method, of class Category.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        Book book = new Book("bla", "bla", "Bla");
+        Category cat = new Category("Bla");
+        boolean expResult = true;
+        int expLength = cat.getBooks().length + 1;
+        boolean result = cat.add(book);
+        assertEquals(expResult, result);
+        assertEquals(expLength, cat.count());
+    }
+
+    /**
+     * Test of remove method, of class Category.
+     */
+    @Test
+    public void testRemove() {
+        System.out.println("remove");
+        Book book = new Book("Bla", "Bla", "Bla");
+        Category cat = new Category("Bla");
+        assertEquals(0, cat.count());
+        cat.add(book);
+                
+        boolean result = cat.remove(book);
+        assertEquals(true, result);
+        assertEquals(0, cat.count());
+    }
+
+    /**
+     * Test of getBooks method, of class Category.
+     */
+    @Test
+    public void testGetBooks() {
+        System.out.println("getBooks");
+        
+        Category cat = new Category("Comics");
+        Book b1 = new Book("Spiderman", "123", "abc");
+        cat.add(b1);
+        Book b2 = new Book("Superman", "123", "abc");
+        cat.add(b2);
+        Book b3 = new Book("Batman", "123", "abc");
+        cat.add(b3);
+        
+        Book[] books = cat.getBooks();
+        assertEquals(b1, books[0]);
+        assertEquals(b2, books[1]);
+        assertEquals(b3, books[2]);
+    }
+
+    /**
+     * Test of count method, of class Category.
+     */
+    @Test
+    public void testCount() {
+        System.out.println("count");
+        Category instance = new Category("Comics");
+        int expResult = 0;
+        int result = instance.count();
+        assertEquals(expResult, result);
+        
+        Book b1 = new Book("Spiderman", "123", "abc");
+        instance.add(b1);
+        assertEquals(1, instance.count());
+    }
 }
